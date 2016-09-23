d00358a23ce2be1473c23da5fcda4dd8abaf700b
Mikhail Kruglyakov <m.kruglyakov@gmail.com>
2016-09-23 13:28:59 +0200

    Add LICENSE Remove some files

--- Distributed_FFT.F90
+++ Distributed_FFT.F90
@@ -1,3 +1,21 @@
+!Copyright (c) 2016 Mikhail Kruglyakov 
+!This file is part of GIEM2G.
+!
+!GIEM2G is free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 2 of the License.
+!
+!GIEM2G is distributed in the hope that it will be useful,
+!but WITHOUT ANY WARRANTY; without even the implied warranty of
+!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!GNU General Public License for more details.
+!
+!You should have received a copy of the GNU General Public License
+!along with GFMRES.  If not, see <http://www.gnu.org/licenses/>.
+!
+!
+!
+
 MODULE DISTRIBUTED_FFT_MODULE
 !VERY VERY C-LIKE MODULE
 !THERE ARE A LOT OF POINTERS FOR THE SAME MEMORY
--- GFGMRES/gmres.F90
+++ GFGMRES/gmres.F90
@@ -1,3 +1,17 @@
+!Copyright (c) 2016 Mikhail Kruglyakov 
+!This file is part of GFGMRES.
+
+!GFGMRES is free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 2 of the License.
+
+!GFGMRES is distributed in the hope that it will be useful,
+!but WITHOUT ANY WARRANTY; without even the implied warranty of
+!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!GNU General Public License for more details.
+
+!You should have received a copy of the GNU General Public License
+!along with GFMRES.  If not, see <http://www.gnu.org/licenses/>.
 MODULE FGMRES
       USE UTILITIES
       USE FGMRES_INTERFACES
